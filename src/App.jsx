e633e8de5e65cff62ee578cc2d2d3d9eefc34324
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-08-26 18:30:43 -0700

    creating jsx navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+	return (
+		<>
+			<div className='container'>
+				<header className='header'>
+					<Navbar />
+				</header>
+			</div>
+			{/* <Hero /> */}
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import './Navbar.scss';
+
+function Navbar() {
+	return (
+		<nav className='navbar'>
+			<div>Burger</div>
+			<h1>
+				<Link href=''>Fe</Link>
+			</h1>
+			<div>Sun</div>
+		</nav>
+	);
+}
+
+export default Navbar;
