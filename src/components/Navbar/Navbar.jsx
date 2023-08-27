d997ba4b2fef9a727d85bdf82de449ef7958626d
Felipe Gonzalez <fgonzalezcare@gmail.com>
2023-08-26 18:48:03 -0700

    creating jsx navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
-import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
 function Navbar() {
 	return (
 		<nav className='navbar'>
-			<div>Burger</div>
-			<h1>Fe</h1>
-			<div>Sun</div>
+			<div className='navbar__burger'>Burger</div>
+			<h1 className='navbar__name'>Fe</h1>
+			<div className='navbar__theme'>Sun</div>
 		</nav>
 	);
 }
