30a77215b027ea73a15e9469e7139c15f0b43ebd
Felipe Gonzalez <pipegonzalez@Felipe-MacBook-Pro.local>
2023-12-20 23:24:33 -0800

    playing around with the read more button

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,17 +4,20 @@ import { Fade, Slide } from "react-awesome-reveal";
 import { useEffect, useRef, useState } from "react";
 import Typed from "typed.js";
 
-const paragraphStyles = {
-  WebkitLineClamp: 1,
-  WebkitBoxOrient: "vertical",
-  overflow: "hidden",
-  display: "-webkit-box",
-};
-
 function Intro() {
   const titleTyped = useRef(null);
 
   const [isOpen, setIsOpen] = useState(false);
+
+  const isMobile = window.matchMedia("(max-width: 720px)").matches;
+
+  const mobileParagraphStyles = isMobile ? {
+    WebkitLineClamp: 4,
+    WebkitBoxOrient: "vertical",
+    overflow: "hidden",
+    display: "-webkit-box",
+  } : {};
+
   useEffect(() => {
     const options = {
       strings: ["Hi there!", "Hola!", "Howdy!", "Hello!"],
@@ -42,30 +45,26 @@ function Intro() {
               <span ref={titleTyped}></span>
             </h1>
 
-            <p
-              className="intro__info--text"
-              
-            >
+            <p className="intro__info--text">
               I'm <mark>Felipe, a Front-end Developer</mark> with a passion for
-              Javascript, ReactJS adn web technologies. Enthusiastic, hardworking, and innovative, I am driven by a relentless desire to learn and explore novel ideas. Presently located in the vibrant city of
+              Javascript, ReactJS and web technologies. Enthusiastic, hardworking and innovative. I am driven by a relentless desire to learn and explore novel ideas. Presently located in the vibrant city of
               Vancouver where I'm available for full-time or freelance work
               opportunities, as well as working remotely for clients worldwide.
+            </p>
+
+            <p className="intro__info--text" style={isOpen ? null : mobileParagraphStyles}>
               <br />
-			  </p>
-			  <p className="intro__info--text" style={isOpen ? null : paragraphStyles}>
-              <br />
-			  I take a personal and creative approach to solving my
+              I take a personal and creative approach to solving my
               clients' problems - whether that's developing a web app, landing
-              page or managing a creative project from start to finish. My
+              page, or managing a creative project from start to finish. My
               strength lies in almost ten years of experience working across the
               entire digital design and development process, as well as my
               self-motivated and collaborative nature.
             </p>
-            
-              <button className="read-more-btn" onClick={() => setIsOpen(!isOpen)}>
-                {isOpen ? "less .." : "read more ..."}
-              </button>
-            
+
+            <button className="read-more-btn" onClick={() => setIsOpen(!isOpen)}>
+              {isOpen ? "less .." : "read more ..."}
+            </button>
           </Fade>
         </div>
         <Slide left triggerOnce>
