c530a31ec9bc5d03fdbe67840f368246abceedce
Felipe Gonzalez <pipegonzalez@Felipe-MacBook-Pro.local>
2024-01-08 11:20:59 -0800

    changed some css styles for mobile and delete a dependency

--- src/components/ProgressBar/SkillsPage.jsx
+++ src/components/ProgressBar/SkillsPage.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// import ProgressBar from './ProgressBar';
-// import { Slide } from 'react-awesome-reveal';
 import Bubbles from '../Bubbles/Bubbles';
 
 function SkillsPage() {
