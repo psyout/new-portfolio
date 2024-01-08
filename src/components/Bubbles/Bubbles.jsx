907ed2b6fe3187ca8204e3cfcb1d095c2a584bea
Felipe Gonzalez <pipegonzalez@Felipe-MacBook-Pro.local>
2024-01-07 17:46:05 -0800

    changed some css styles for mobile and delete a dependency

--- src/components/Bubbles/Bubbles.jsx
+++ src/components/Bubbles/Bubbles.jsx
@@ -2,12 +2,39 @@ import React from 'react';
 import './Bubbles.scss';
 
 function Bubbles() {
-	const mySkills = ['HTML', 'CSS', 'SASS', 'BEM', 'Flexbox', 'Grid Layout', 'Responsive Design', 'Boostrap', 'Tailwind', 'JavaScript', 'React', 'Git', 'Next', 'Node', 'API', 'JSON', 'MySQL', 'Wordpress', 'Hosting', 'Figma', 'Photoshop', 'Illustrator', 'Wireraming']
+	const mySkills = [
+		'HTML',
+		'CSS',
+		'SASS',
+		'BEM',
+		'Flexbox',
+		'Grid Layout',
+		'Responsive Design',
+		'Boostrap',
+		'Tailwind',
+		'JavaScript',
+		'React',
+		'Git',
+		'Next',
+		'Node',
+		'API',
+		'JSON',
+		'MySQL',
+		'Wordpress',
+		'Hosting',
+		'Figma',
+		'Photoshop',
+		'Illustrator',
+		'Wireraming',
+	];
 	return (
 		<>
+			<h4 className='bubbles-title'>Skillset</h4>
 			<ul className='bubbles-list'>
 				{mySkills.map((item) => (
-					<li key={item} className='bubbles-item'>{item}</li>
+					<li key={item} className='bubbles-item'>
+						{item}
+					</li>
 				))}
 			</ul>
 		</>
