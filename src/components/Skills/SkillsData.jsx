aeb88b25f3b19ff771b2f0abc293f98902f8cf7b
Felipe Gonzalez <pipegonzalez@Felipe-MacBook-Pro.local>
2024-01-07 17:28:02 -0800

    changed some css styles for mobile and delete a dependency

--- src/components/Skills/SkillsData.jsx
+++ src/components/Skills/SkillsData.jsx
@@ -3,17 +3,20 @@ import { FaHtml5, FaCss3Alt, FaSquareJs } from 'react-icons/fa6';
 export const SkillsData = [
 	{
 		title: 'Since version 4.0',
-		body: 'My HTML skills prove incredibly useful in React. Writing JSX becomes intuitive, like crafting HTML with JavaScript`s power. I create dynamic components, conditionally render content, and iterate through data arrays seamlessly. This mix of HTML and JavaScript in JSX simplifies React development',
+		body:
+			'My HTML skills are key in React. Writing JSX blends HTML clarity with JavaScript power. I easily create dynamic components, conditionally render content, and iterate through data arrays, making React development simpler.',
 		icon: <FaHtml5 />,
 	},
 	{
 		title: 'Styling for life',
-		body: 'I have mastered the art of responsive design, creating layouts that seamlessly adapt to any screen size. Playing around with animations and transitions has also become second nature, adding that extra touch of life to websites.',
+		body:
+			'I have mastered the art of responsive design, creating layouts that seamlessly adapt to any screen size. Playing around with animations and transitions has also become second nature, adding that extra touch of life to websites.',
 		icon: <FaCss3Alt />,
 	},
 	{
 		title: 'JavaScript, my daily adventure',
-		body: 'Able to create dynamic components, managing state, and handling logic. This leads to responsive, interactive, and high-performance React apps. Learning React Native and Electron.js for mobile apps at the moment. It is like a treasure hunt, with discoveries growing.',
+		body:
+			'Creating dynamic components, managing state, and handling logic result in responsive, interactive, high-performance React apps. Currently exploring React Native for mobile apps, continuously uncovering new discoveries.',
 		icon: <FaSquareJs />,
 	},
 ];
--- src/components/Title/TitleData.jsx
+++ src/components/Title/TitleData.jsx
@@ -3,7 +3,8 @@ export const TitleData = [
 		title: 'check this out',
 		body: (
 			<span>
-				I've been working on some new projects that I'll be updating soon, in the meantime, take a look at what I did when I was doing a <mark>Web Development Bootcamp</mark> a few months ago. Stay tuned for updates, and thanks for your support!
+				I've been working on some new projects that I'll be updating soon, in the meantime, take a look at what I did when I was doing a <mark>Web Development Bootcamp</mark> a few months ago. Stay tuned
+				for updates, and thanks for your support!
 			</span>
 		),
 	},
@@ -11,7 +12,9 @@ export const TitleData = [
 		title: 'these are my skills and more…',
 		body: (
 			<span>
-				During my time at BrainStation,  I gained expertise in a range of pivotal web development languages and frameworks, such as <mark>HTML, CSS, JavaScript, and React.</mark> This experience not only honed my technical skills but also enabled me to craft engaging web interfaces. With React, I improved my ability to develop scalable UI components, while working with <mark>Axios and NodeJS</mark> greatly enhanced my proficiency in Back-End development.
+				During my time at BrainStation, I mastered crucial web development languages and frameworks, such as <mark>HTML, CSS, JavaScript, and React.</mark> This enhanced both my technical skills and
+				empowered me in crafting captivating web interfaces. With React, I enhanced my proficiency in building scalable UI components, while Axios and NodeJS significantly boosted my Back-End development
+				expertise.
 			</span>
 		),
 	},
