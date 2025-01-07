da6e0709d21c36d872189f38b389b8f3ff506939
Felipe Gonzalez <contraviento@gmail.com>
2025-01-06 18:29:27 -0800

    add motion animation hero section and about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,55 @@
 import React from 'react';
 import Button from '../Button/Button';
 import './About.scss';
+import { motion } from 'framer-motion';
 
 function About() {
 	return (
-		<section id='about' className='about'>
-			<div className='about-container'>
-				<h1 className='about-container__title'>01. About me</h1>
-				<p className='about-container__text'>
-					I'm driven by a relentless desire to learn and explore novel ideas. Presently located in the vibrant city of Vancouver where I'm available for full-time or freelance work opportunities, as well
-					as working remotely for clients worldwide.
-					<br />
-					<br />I take a personal and creative approach to solving my clients' problems - whether that's developing a web app, landing page, or managing a creative project from start to finish. My strength
-					lies in my ten years of experience working across the entire digital design and development process, as well as my self-motivated and collaborative nature.
-				</p>
-				<Button title={`Get Resume`} url={`https://www.felipegonzalez.io/felipe-gonzalez-resume.pdf`} />
+		<section id="about" className="about">
+			<div className="about-container">
+				<motion.h1
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					transition={{ duration: 1, delay: 0 }}
+					viewport={{ once: true }}
+					repeat={Infinity}
+					className="about-container__title">
+					01. About me
+				</motion.h1>
+
+				<motion.p
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					transition={{ duration: 1, delay: 0.5 }}
+					viewport={{ once: true }}
+					className="about-container__text">
+					I'm driven by a relentless desire to learn and explore novel ideas. Presently
+					located in the vibrant city of Vancouver where I'm available for full-time or
+					freelance work opportunities, as well as working remotely for clients worldwide.
+				</motion.p>
+				<motion.p
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					transition={{ duration: 1, delay: 1 }}
+					viewport={{ once: true }}
+					className="about-container__text">
+					I take a personal and creative approach to solving my clients' problems -
+					whether that's developing a web app, landing page, or managing a creative
+					project from start to finish. My strength lies in my ten years of experience
+					working across the entire digital design and development process, as well as my
+					self-motivated and collaborative nature.
+				</motion.p>
+
+				<motion.div
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					transition={{ duration: 1, delay: 1.5 }}
+					viewport={{ once: true }}>
+					<Button
+						title={`Get Resume`}
+						url={`https://www.felipegonzalez.io/felipe-gonzalez-resume.pdf`}
+					/>
+				</motion.div>
 			</div>
 		</section>
 	);
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,44 +1,63 @@
 import './Intro.scss';
-import { useEffect, useRef } from 'react';
-import Typed from 'typed.js';
 import ProfilePicture from '../../assets/images/profile-picture-square.jpg';
+import { motion } from 'motion/react';
+import { TypeAnimation } from 'react-type-animation';
 
 function Intro() {
-	const titleTyped = useRef(null);
-
-	useEffect(() => {
-		const options = {
-			strings: ['Hi there!', 'Hola!', 'Howdy!', 'Hello!'],
-			typeSpeed: 150,
-			backSpeed: 70,
-			backDelay: 2000,
-			loop: false,
-			showCursor: false,
-			cursorChar: '&nbsp;',
-		};
-
-		const typed = new Typed(titleTyped.current, options);
-
-		return () => {
-			typed.destroy();
-		};
-	}, []);
-
 	return (
-		<section id='intro' className='intro'>
-			<div className='intro__info'>
-				<h1 className='intro__info--title'>
-					<span ref={titleTyped}></span>
-				</h1>
+		<section id="intro" className="intro">
+			<div className="intro__info">
+				<motion.div
+					className="intro__info--title"
+					initial={{ opacity: 0, y: -50 }}
+					whileInView={{ opacity: 1, y: 0 }}
+					viewport={{ once: true }}
+					transition={{ duration: 1 }}>
+					<TypeAnimation
+						sequence={[
+							'Front-end Developer',
+							1000,
+							'Web Designer',
+							1000,
+							'Enthusiast',
+							1000,
+						]}
+						speed={10}
+						repeat={Infinity}
+						className="intro__info--title"
+					/>
+				</motion.div>
 
-				<p className='intro__info--text'>
-					I'm <mark>Felipe, a Front-end Developer</mark>. Building simple and beautiful things for complex interfaces is what I enjoy most.
-				</p>
+				<motion.p
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					transition={{ duration: 1, delay: 0.5 }}
+					viewport={{ once: true }}
+					className="intro__info--title">
+					Hey there! I'm <mark>Felipe,</mark>
+				</motion.p>
+
+				<motion.p
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					transition={{ duration: 1, delay: 1 }}
+					viewport={{ once: true }}
+					className="intro__info--text">
+					Building simple and beautiful things for complex interfaces is what I enjoy
+					most.
+				</motion.p>
 			</div>
 
-			<div className='intro__placeholder'>
-				<img className='intro__placeholder--img' src={ProfilePicture} alt='Profile' />
-			</div>
+			<motion.div
+				initial={{ opacity: 0, scale: 0 }}
+				animate={{ opacity: 1, scale: 1 }}
+				transition={{
+					duration: 0.4,
+					scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 },
+				}}
+				className="intro__placeholder">
+				<img className="intro__placeholder--img" src={ProfilePicture} alt="Profile" />
+			</motion.div>
 		</section>
 	);
 }
